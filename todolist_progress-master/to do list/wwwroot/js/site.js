f3f1fb9af5e5c844f8bffb1c7e59dd6d3c31d734
Caritos_Kristopher_BSIT32E3 <161918390+caritos-kristopher-BSIT32E3@users.noreply.github.com>
2024-04-10 13:13:05 +0800

    Add files via upload

---
+++ todolist_progress-master/to do list/wwwroot/js/site.js
@@ -0,0 +1,55 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+import { Button } from "../lib/bootstrap/dist/js/bootstrap.esm";
+import './site.css';
+import './index.cshtml';
+
+<environment include="Development">
+    <link rel="stylesheet" href="~/lib/bootstrap/dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="~/css/site.css" />
+</environment>
+
+
+
+// Write your JavaScript code.
+
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+let taskCount = 0;
+
+const displayCount = (taskCount) => {
+    countValue.innerText = taskCount;
+};
+
+const addTask = () => {
+    const taskName = newTaskInput.value.trim();
+    error.style.display = "none";
+    if (!taskName) {
+        setTimeout(() => {
+            error.style.display = "block";
+        }, 200);
+        return;
+    }
+
+    const task = <div class="task">
+        <input type="checkbox" class="task-check">
+            <span class="taskname">${taskName}</span>
+            <button class="edit">
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>)
+
+            <button class="delete">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </input>
+    </div>;
+
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+
+};
+
+addBtn.addEventListener("click", addTask);
